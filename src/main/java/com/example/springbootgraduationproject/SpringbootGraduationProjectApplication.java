be6a3ee8de50ec1629a45c80793ca956c61183fc
win10 <2356443494@qq.com>
2020-03-16 15:19:50 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootgraduationproject/SpringbootGraduationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgraduationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraduationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGraduationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootgraduationproject/SpringbootGraduationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootgraduationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraduationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
